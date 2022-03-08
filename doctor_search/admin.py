d963e5051f78b0c2e662446583da15e6742d868f
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-08 18:46:10 +0000

    Register all new models to admin.py

--- doctor_search/admin.py
+++ doctor_search/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Profile)
+admin.site.register(County)
+admin.site.register(District)
+admin.site.register(Neighborhood)
+admin.site.register(Address)
+admin.site.register(DayWeek)
+admin.site.register(Rating)
+admin.site.register(Speciality)

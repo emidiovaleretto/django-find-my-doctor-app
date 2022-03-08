72c8456dc2f038fce5705d81303cfe1a0b802047
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-08 18:44:19 +0000

    Create County Models

---
+++ doctor_search/models/models_county.py
@@ -0,0 +1,11 @@
+from doctor_search.models import *
+
+
+class County(models.Model):
+    name = models.CharField(null=False, max_length=20)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

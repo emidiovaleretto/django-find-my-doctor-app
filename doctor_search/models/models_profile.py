a273dd9ba5bfcbd7ddc8e27e7b0aba4ef5c8fc14
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-17 00:48:12 +0000

    Fix a typo.

--- doctor_search/models/models_profile.py
+++ doctor_search/models/models_profile.py
@@ -2,10 +2,10 @@ from doctor_search.models import *
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rode = models.IntegerField(choices=ROLE_CHOICE, default=3)
+    role = models.IntegerField(choices=ROLE_CHOICE, default=3)
     birthday = models.DateField(default=None, null=True, blank=True)
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
     token = models.CharField(max_length=255, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     favorites = models.ManyToManyField(User, blank=True, related_name='favorites')

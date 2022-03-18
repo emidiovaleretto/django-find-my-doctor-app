d5ad43081877a098b22729cf99d80e94868b19f1
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-18 17:47:19 +0000

    Add a paginator logic.

--- doctor_search/views/DoctorViews.py
+++ doctor_search/views/DoctorViews.py
@@ -1,35 +1,43 @@
-from django.http import HttpResponse
-from doctor_search.models import Profile
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from doctor_search.models.models_profile import Profile
+from doctor_search.models.models_rating import Rating
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def list_doctor_view(request):
-    name = request.GET.get('name')
-    speciality = request.GET.get('speciality')
-    neighborhood = request.GET.get('neighborhood')
-    district = request.GET.get('district')
-    county = request.GET.get('county')
+    name = request.GET.get("name")
+    speciality = request.GET.get("speciality")
+    neighborhood = request.GET.get("neighborhood")
+    district = request.GET.get("district")
+    county = request.GET.get("county")
 
-    doctors = Profile.objects.filter(role=3).all()
-
+    doctors = Profile.objects.filter(role=2)
     if name is not None and name != '':
-        doctors = doctors.filter(
-            Q(user__first_name__contains=name) |
-            Q(user__username__contains=name)
-        )
+        doctors = doctors.filter(user__first_name__contains=name)
     if speciality is not None:
-        doctors = doctors.filter(specialities__id=speciality)
+        doctors = doctors.filter(specialties__id=speciality)
 
     if neighborhood is not None:
         doctors = doctors.filter(addresses__neighborhood=neighborhood)
     else:
         if district is not None:
-            doctors = doctors.filter(
-                addresses__neighborhood__district=district)
+            doctors = doctors.filter(addresses__neighborhood__district=district)
         elif county is not None:
-            doctors = doctors.filter(
-                addresses__neighborhood__district__county=county)
+            doctors = doctors.filter(addresses__neighborhood__city__county=county)
 
-    print(doctors.all())
+    if len(doctors) > 0:
+        paginator = Paginator(doctors, 8)
+        page = request.GET.get('page')
+        doctors = paginator.get_page(page)
 
-    return HttpResponse('Listing one or more doctors.')
+    get_copy = request.GET.copy()
+    parameters = get_copy.pop('page', True) and get_copy.urlencode()
+
+    context = {
+        'doctors': doctors,
+        'parameters': parameters
+    }
+
+    return render(request, template_name='doctors/doctors.html', context=context, status=200)

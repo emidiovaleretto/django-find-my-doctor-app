40a5c6a0b0f3e5db9bc0a69a01a940aa200d0cb0
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-30 17:52:28 +0000

    Implement list_profile_view function.

--- doctor_search/views/ProfileViews.py
+++ doctor_search/views/ProfileViews.py
@@ -1,8 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from doctor_search.models import Profile
+from django.core.paginator import Paginator
+
 
 def list_profile_view(request, id=None):
+    profile = None
     if id is None and request.user.is_authenticated:
-        id = request.user.id
+        profile = Profile.objects.filter(user=request.user)[0]
+    elif id is not None:
+        profile = Profile.objects.filter(user__id=id)[0]
     elif not request.user.is_authenticated:
-        id = 0
-    return HttpResponse(f'<h1>User id: {id}</h1>')
+        return redirect(to='/')
+
+    favorites = profile.show_favorites()
+
+    if len(favorites) > 0:
+        paginator = Paginator(favorites, 8)
+        page = request.GET.get('page')
+        favorites = paginator.get_page(page)
+
+    context = {
+        'profile': profile,
+        'favorites': favorites
+    }
+
+    return render(request, 'profile/profile.html', context=context, status=200)

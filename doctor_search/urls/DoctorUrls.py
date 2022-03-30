953848ac323f698e34cfc8f189b77e61c5bb5221
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-30 17:55:51 +0000

    Add route to favorite views.

--- doctor_search/urls/DoctorUrls.py
+++ doctor_search/urls/DoctorUrls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from doctor_search.views.DoctorViews import list_doctor_view
+from doctor_search.views.DoctorViews import list_doctor_view, add_favorite_view
 
 
 urlpatterns = [
-    path('', list_doctor_view, name='doctors')
+    path('', list_doctor_view, name='doctors'),
+    path('favorite', add_favorite_view, name='doctor-favorite')
 ]

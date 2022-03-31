8e5559580f4c2ea386e3a99bd9dd6721c8e0d0ea
Emidio Valeretto <emidio.valereto@gmail.com>
2022-03-31 11:52:14 +0000

    Add a url path to the edit profile function.

--- doctor_search/urls/ProfileUrls.py
+++ doctor_search/urls/ProfileUrls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from doctor_search.views.ProfileViews import list_profile_view
+from doctor_search.views.ProfileViews import list_profile_view, edit_profile
 
 
 urlpatterns = [
     path('', list_profile_view, name='profiles'),
-    path('<int:id>/', list_profile_view, name='profile')
+    path('<int:id>/', list_profile_view, name='profile'),
+    path('edit', edit_profile, name='edit-profile'),
 ]

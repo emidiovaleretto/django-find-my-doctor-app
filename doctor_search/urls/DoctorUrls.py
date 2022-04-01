4639d871bdde98e5b644b6af924ea3f16e02b107
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-01 15:04:08 +0000

    Register a url path for rating doctor views.

--- doctor_search/urls/DoctorUrls.py
+++ doctor_search/urls/DoctorUrls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from doctor_search.views.DoctorViews import list_doctor_view, add_favorite_view, remove_favorite_view
+from doctor_search.views.DoctorViews import list_doctor_view, add_favorite_view, remove_favorite_view, rate_doctor
 
 
 urlpatterns = [
     path('', list_doctor_view, name='doctors'),
     path('favorite', add_favorite_view, name='doctor-favorite'),
     path('favorite/remove', remove_favorite_view, name='remove-doctor-favorite'),
+    path('rating/<doctor_id:int>', rate_doctor(request), name='rating-doctor'),
 ]

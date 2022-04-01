45b62ffe88203435311ac56a2363d60a1c2e8d22
Emidio Valeretto <emidio.valereto@gmail.com>
2022-04-01 17:56:35 +0000

    Add a url path for doctor rating.

--- doctor_search/urls/DoctorUrls.py
+++ doctor_search/urls/DoctorUrls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', list_doctor_view, name='doctors'),
     path('favorite', add_favorite_view, name='doctor-favorite'),
     path('favorite/remove', remove_favorite_view, name='remove-doctor-favorite'),
-    path('rating/<doctor_id:int>', rate_doctor(request), name='rating-doctor'),
+    path('rating/<int:doctor_id>', rate_doctor, name='rating-doctor'),
 ]
